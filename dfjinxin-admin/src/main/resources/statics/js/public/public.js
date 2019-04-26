3c03b0d09d3046297e7a608cdc2bd33ee182cc3e
HDLR <492587402@qq.com>
2019-04-26 23:22:42 +0800

    修改

---
+++ dfjinxin-admin/src/main/resources/statics/js/public/public.js
@@ -0,0 +1,33 @@
+function ajaxFunctionPost(url, data, successFunctionCall, errorFunctionCall){
+    $.ajax({
+        type: "POST",
+        url: baseURL + url,
+        data: data,
+        dataType: "json",
+        success: function(resPublic){
+            if (successFunctionCall != null) {
+                successFunctionCall(resPublic);
+            }
+        },
+        error: function(resPublic){
+            if(resPublic.code !== 0 && null != resPublic.msg){
+                //this.$message.error(resPublic.msg)
+                vm.$message({
+                    showClose: true,
+                    message: resPublic.msg,
+                    type: 'error'
+                });
+            }else{
+                //this.$message.error("系统请求异常，请联系管理人员定位")
+                vm.$message({
+                    showClose: true,
+                    message: '系统请求异常，请联系管理人员定位',
+                    type: 'error'
+                });
+            }
+            if (errorFunctionCall != null) {
+                errorFunctionCall(resPublic);
+            }
+        }
+    });
+}
